342262afdd1ce27df704e836f1f2c24e46c7a579
AlekseyPriz <aleksey.priz@gmail.com>
2017-12-29 22:19:22 +0300

    Настройка порта вынесена из файла server.js в отдельный скрипт normalizePort-2

--- angular/ctrl/gogolCtrl.js
+++ angular/ctrl/gogolCtrl.js
@@ -30,13 +30,12 @@ app.controller('exchangersCtrl', function ($scope, $http) {
     }
   };
 
-  // let host = 'https://evening-falls-64086.herokuapp.com';
-  let host = 'http://localhost:3000';
+  let host = 'https://evening-falls-64086.herokuapp.com';
+  // let host = 'http://localhost:3000';
 
 
   $scope.getExchangeRate = function (from, to) {
     $http.get(host +'/exchange/'+ from + '/' + to)
-//    $http.get('http://localhost:3000/exchange/'+ from + '/' + to)
       .then(function (result) {
         console.log('Курсы получены', result.data);
         $scope.exchs = result.data;
@@ -73,7 +72,6 @@ app.controller('exchangersCtrl', function ($scope, $http) {
 
 
   $http.get(host + '/start')
-//  $http.get('http://localhost:3000/start')
     .then(function (result) {
       console.log('Курсы получены', result.data);
       $scope.exchs = result.data;
